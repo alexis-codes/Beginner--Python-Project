d4ebbc4839d8e961da434d5160499202bca4b077
alexis-codes <alexisgogo49@gmail.com>
2025-02-03 16:07:53 +0300

    initial commit

---
+++ variable.py
@@ -0,0 +1,22 @@
+#A variable is a container used to store information
+location = "Westlants"
+firstname = "Alexis"
+lastname = "Gogo"
+school = "eMobilis"
+x = 39
+y=2
+a=78
+
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+
+print(firstname, lastname)
+print("My full name is", firstname, lastname)
+print(school, "is a coding school")
+print(x + y)
+
+
+#Assignment: The product of 39 and 2 is 78
+print("The product of", x, "and", y, "is", a)
