335e0e4e9a6c53c6425d9e962a12eb4456a35964
vivitan01 <nyamburahuuny@gmail.com>
2025-02-05 16:29:16 +0300

    initial commit

--- assignmaent.py
+++ assignmaent.py
@@ -1,8 +1,18 @@
 #A simple python project to
 #check whether a year is a leap year on not
-
+year = int(input("Enter Year "))
+if year % 4 == 0 or year % 100 == 0 or year % 400 == 0 :
+    print (year,"is a leap year")
+else :
+    print(year,"is not leap")
 
 
 
 
 #A letter is a vowel or a consonant
+x = str(input("Type letter "))
+if (x == 'a' or x == 'e' or
+        x == 'i' or x == 'o' or x == 'u'):
+    print("Vowel")
+else:
+    print("Consonant")
